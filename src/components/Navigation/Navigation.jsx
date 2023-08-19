34fc9b39e1370de4321aa2fccd4d57acde10ee5e
nurealammiaji <nurealam.miaji@gmail.com>
2023-08-19 13:59:55 +0600

    active link indicatior added

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -8,9 +8,9 @@ const Navigation = () => {
                 <h3 className="text-2xl font-bold">EP Auth</h3>
             </div>
             <div className="font-semibold [&>*]:px-4 [&>*]:py-2 hover:[&>*]:bg-green-600 [&>*]:rounded-2xl hover:[&>*]:text-white">
-                <NavLink to="/">Home</NavLink>
-                <NavLink to="/register">Register</NavLink>
-                <NavLink to="/login">Login</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'bg-green-600 text-white' : ''} to="/">Home</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'bg-green-600 text-white' : ''} to="/register">Register</NavLink>
+                <NavLink className={({isActive}) => isActive ? 'bg-green-600 text-white' : ''} to="/login">Login</NavLink>
             </div>
         </nav>
     );
